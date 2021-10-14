b4200b62b73ee6572a3f0bb7a8a699c811f1ce45
Alex <acanellas@protonmail.com>
2021-10-14 10:42:02 -0400

    netincomeloss functionality

--- xbrl-parser.c
+++ xbrl-parser.c
@@ -10,7 +10,7 @@ char* out_filename;
 
 
 int main(int argc, char* argv[]) {
-
+/*
     int option;
     opterr = 0;
     while ( (option = getopt(argc, argv, ":o:r:g:b:t")) != -1 ) {
@@ -24,28 +24,39 @@ int main(int argc, char* argv[]) {
     for (int i = optind; i < argc; i++) {
         in_filename = argv[i];
     }
+    */
 
     //open input file and check for errors
-    FILE* in_file = fopen(in_filename, "rb");
+    FILE* in_file = fopen("ALB_10-K_2020.xml", "rb");
     if (in_file == NULL) {
         perror("ERROR: Could not open input file \n");
         return EXIT_FAILURE;
     }
 
     // create net income loss tag string for reference (including the space at the end)
-    char netIncomeLossTag[23] = "<us-gaap:NetIncomeLoss ";
+    char netIncomeLossTag[24] = "<us-gaap:NetIncomeLoss ";
     // create compare string
-    char compareTag[23];
+    char compareTag[24];
     // create variable to store desired value
     char netIncomeLoss[9];
     // fread 23 bytes into compare string
-    fread(compareTag, sizeof(char), 23, in_file);
-    // compare with reference string. if match, skip until after ">" is found, read in the next 9 bytes
-    if(strcmp(compareTag, netIncomeLossTag) == 0) {
-        fscanf(in_file, "%*[^>]");
+    while (fgets(compareTag, sizeof(compareTag), in_file) != NULL) {
+        while(strcmp(compareTag, netIncomeLossTag) != 0) {
+            fgets(compareTag, sizeof compareTag, in_file);
+        }
+
+        printf("DEBUG: compareTag = %s\n", compareTag);
+
+        // compare with reference string. if match, skip until after ">" is found, read in the next 9 bytes
+        if(strcmp(compareTag, netIncomeLossTag) == 0) {
+            char c = fgetc(in_file);
+            while(c != '>') {
+                c = fgetc(in_file);
+            }
+            fgets(netIncomeLoss, 9, in_file);
+            printf("Net income loss: %s\n", netIncomeLoss);
+        }
     }
-    // else, skip to next line, repeat
-
-
+    fclose(in_file);
 }
 
